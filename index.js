51ac760ed4581d8b6ecd8b51dc65cc91a50b08c8
Derafino <61626827+Derafino@users.noreply.github.com>
2023-11-28 00:18:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
   console.log("Just got a request!")
-  res.sendFile(__dirname + '/inex.html');
+  res.sendFile(__dirname + '/index.html');
 })
 app.listen(process.env.PORT || 3000)
